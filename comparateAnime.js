a054ad521a2373870f16677e23ce305fc1614374
erfffff <dakevcraft@yahoo.fr>
2019-11-29 10:18:30 +0100

    notif ok

--- comparateAnime.js
+++ comparateAnime.js
@@ -7,6 +7,14 @@ function delay(timeout) {
       setTimeout(resolve, timeout);
     });
 }
+function replaceContents(file, replacement, cb) {
+
+  fs.readFile(replacement, (err, contents) => {
+    if (err) return cb(err);
+    fs.writeFile(file, contents, cb);
+  });
+
+}
 
 async function comparateAnime(bot, msg)
 {
@@ -18,6 +26,13 @@ async function comparateAnime(bot, msg)
 
 	let contentAddAnime = fs.readFileSync('./DatabaseList/ListeAnime.json');
 	let parsedAddAnime = JSON.parse(contentAddAnime);
+
+	// read TempListeAnime and put the new data of the anime
+	let contentAddAnimeTemp = fs.readFileSync('./DatabaseList/TempListeAnime.json');
+	let parsedAddAnimeTemp = JSON.parse(contentAddAnimeTemp);
+
+	let changeData = false
+
     for (let j=0; j<DataAnime.length; j++)
     {
         for (let m=0; m<parsedAddAnime.length; m++)
@@ -25,7 +40,7 @@ async function comparateAnime(bot, msg)
             if ((DataAnime[j].en == parsedAddAnime[m].name_anime))
             {
             	//print the anime name, the data inside ListAnime.json and the real data online
-				/*console.log("name\t" + parsedAddAnime[m].name_anime)
+/*				console.log("name\t" + parsedAddAnime[m].name_anime)
 				console.log("bdd\t" + parsedAddAnime[m].number_anime)
 				console.log("irl\t" + DataAnime[j].next_epiosode.replace(":",""))*/
 
@@ -34,55 +49,39 @@ async function comparateAnime(bot, msg)
                     //notify the user
 					msg("Hey <@!" +  parsedAddAnime[m].user_id + ">, " + "l'épisode numéro " + DataAnime[j].next_epiosode.replace(":", "") + " de " + parsedAddAnime[m].name_anime + " vient de sortir !")
 
-					// read TempListeAnime and put the new data of the anime
-                    let contentAddAnimeTemp = fs.readFileSync('./DatabaseList/TempListeAnime.json');
-					let parsedAddAnimeTemp = JSON.parse(contentAddAnimeTemp);
-                    let new_list_anime = { user_id: parsedAddAnime[m].user_id , name_anime: parsedAddAnime[m].name_anime, number_anime: DataAnime[j].next_epiosode.replace(":", "")};
+					//add the new data inside an array
+                    let new_list_anime = { user_id: parsedAddAnime[m].user_id , name_anime: parsedAddAnime[m].name_anime, number_anime: DataAnime[j].next_epiosode.replace(":", "")}
                     parsedAddAnimeTemp.push(new_list_anime);
-                    let JSON_animeTemp = JSON.stringify(parsedAddAnimeTemp);
-                    fs.writeFile("./DatabaseList/TempListeAnime.json", JSON_animeTemp, function(err, result) {
-                        if(err) console.log('error', err);
-                    });
-
-                    // replace the content of the old data (ListeAnime) by the new data (TempListeAnime)
-                    function replaceContents(file, replacement, cb) {
-
-					  fs.readFile(replacement, (err, contents) => {
-					    if (err) return cb(err);
-					    fs.writeFile(file, contents, cb);
-					  });
-
-					}
-
-					// replace contents of file './DatabaseList/ListeAnime.json' with contents of './DatabaseList/TempListeAnime.json'
-					replaceContents("./DatabaseList/ListeAnime.json", "./DatabaseList/TempListeAnime.json", err => {
-					  if (err) {
-					    // handle errors here
-					    throw err;
-					  }
-					});
-
-					// delete the Temp file
-					fs.unlink('./DatabaseList/TempListeAnime.json', function (err) {
-					  if (err) throw err;
-					});
-
-					// create the Temp file
-                    fs.appendFile('./DatabaseList/TempListeAnime.json', '[]', function (err) {
-					  if (err) throw err;
-					});
-					// empty the content of TempListeAnime
-/*					let contentAddAnimedelete = fs.readFileSync('./DatabaseList/TempListeAnime.json');
-					let parsedAddAnimedelete = JSON.parse(contentAddAnimedelete);
-					let emptyTempListe = []
-					parsedAddAnimedelete.push(emptyTempListe);
-                    let JSON_animedelete = JSON.stringify(parsedAddAnimedelete);
-                    fs.writeFile("./DatabaseList/TempListeAnime.json", JSON_animedelete, function(err, result) {
-                        if(err) console.log('error', err);
-                    });*/
+                    changeData = true
 				}
 			}
 		}
+	}
+
+	if(changeData == true)
+	{
+		//transform the array into a json file
+	    let JSON_animeTemp = JSON.stringify(parsedAddAnimeTemp);
+	    //write in the file
+	    fs.writeFile("./DatabaseList/TempListeAnime.json", JSON_animeTemp, function(err, result) {
+	        if(err) console.log('error', err);
+	    });
+		// replace the content of the old data (ListeAnime) by the new data (TempListeAnime)
+		// replace contents of file './DatabaseList/ListeAnime.json' with contents of './DatabaseList/TempListeAnime.json'
+		replaceContents("./DatabaseList/ListeAnime.json", "./DatabaseList/TempListeAnime.json", err => {
+		  if (err) {
+		    // handle errors here
+		    throw err;
+		  }
+		});
+
+		replaceContents("./DatabaseList/TempListeAnime.json", "./DatabaseList/EmptyFile.json", err => {
+		  if (err) {
+		    // handle errors here
+		    throw err;
+		  }
+		});
+		changeData = false
 	}
 }
 exports.notif = (bot, msg) => { var varnotif = setInterval(comparateAnime, 5000, bot, msg); }
