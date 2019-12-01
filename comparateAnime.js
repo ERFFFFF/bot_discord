8444805fd99f2e0a08bc005e133fcb0977fbec95
erfffff <dakevcraft@yahoo.fr>
2019-12-01 20:21:54 +0100

    del useless lines comparateAnime

--- comparateAnime.js
+++ comparateAnime.js
@@ -2,12 +2,7 @@
 const AnimeFunc = require('./animeFunc.js');
 var fs = require("fs");
 const delay = require('./delay.js');
-// delay.delay, sleep, wait function.
-/*function delay(timeout) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, timeout);
-    });
-}*/
+
 function replaceContents(file, replacement, cb) {
 
   fs.readFile(replacement, (err, contents) => {
