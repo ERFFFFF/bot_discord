a28eb12acaea72fef21f2ebed31abed94d9f879c
unknown <dakevcraft@yahoo.fr>
2020-07-20 18:02:52 +0200

    rework MEMO and rework syntaxe

--- bot.js
+++ bot.js
@@ -6,152 +6,152 @@ const bot = new Discord.Client({});
 const bot_settings = require('./settings/bot_settings.json');
 
 // created commands
-const AnimeFunc = require('./animeFunc.js'); 
+const AnimeFunc = require('./animeFunc.js');
 const MangaFunc = require('./mangaFunc.js');
 const notifAnime = require('./comparateAnime.js');
-const delay = require("./delay.js");
-const anime = require("./anime.js");
-const cctl = require("./cctl.js");
-const prune = require("./prune.js");
-const memo = require("./memo.js");
-const help = require("./help.js");
-const restart = require("./restart.js");
+const delay = require('./delay.js');
+const anime = require('./anime.js');
+const cctl = require('./cctl.js');
+const prune = require('./prune.js');
+const memo = require('./memo.js');
+const help = require('./help.js');
+const restart = require('./restart.js');
 
 // File reader, writer
-var fs = require("fs");
+var fs = require('fs');
 //MUSIC BOT
-var opusscript = require("opusscript");
-bot.music = require("discord.js-musicbot-addon");
+var opusscript = require('opusscript');
+bot.music = require('discord.js-musicbot-addon');
 
 // scraper for MyAnimeList
-const malScraper = require('mal-scraper')
+const malScraper = require('mal-scraper');
 
 //Prefix for the Botbrowser
 const PREFIX = ',';
 
-
 // connection to the database
 const MongoClient = require('mongodb').MongoClient;
-const NameDB = bot_settings.DatabaseNameMongo
+const NameDB = bot_settings.DatabaseNameMongo;
 const uri = `mongodb+srv://${bot_settings.UsernameMongo}:${bot_settings.PasswordMongo}@cluster0.skkqx.mongodb.net/${NameDB}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    console.log("Connected to Atlas Mongo.")
-    const db = client.db(NameDB);
-    const collection = db.collection("test");
-    collection.insertOne( { item: "toto", qty: 15 } )
-    // perform actions on the collection object
-    
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
-client.close();
 
 var list_AddNomAnime = 0;
 
-function cl(Message)
-{
-    console.log(Message)
+function cl(Message) {
+  console.log(Message);
 }
 
-bot.on('ready', () => 
-{
-    var channel = bot.channels.get('589224223470256129')
-    function msg(msgs)
-    {
-        channel.send(msgs);
-    }
-    notifAnime.notif(bot, msg)
-    
-    bot.user.setActivity('Peter des gueules');
+bot.on('ready', () => {
+  var channel = bot.channels.get('589224223470256129');
+  function msg(msgs) {
+    channel.send(msgs);
+  }
+  //notifAnime.notif(bot, msg);
+
+  bot.user.setActivity('Peter des gueules');
 });
-
-bot.on('message', async message => 
-{    
-    function msgs(msg)
-    {
-        message.channel.send(msg);
-    }
-
-    //¨PRIVATE MESSAGE
-    // erfffff
-    //bot.users.get("157510824426995714").send("poulet")
-    // hiroi
-    //bot.users.get("160835265928232961").send("poulet")
-
-    if(!message.content.startsWith(PREFIX)) return;
-    if(message.author.bot) return;
-    if(message.content.toString() === `${PREFIX}rs`) 
-    {
-        restart.rs(bot, msgs, message)
-    }
-    if(message.content.toString() === `${PREFIX}test`) 
-    {
-        let test = 'Ceci est un test. oui';
-        msgs(test);
-    }
-    if(message.content.toString() === `${PREFIX}random`) 
-    {
-        msgs(Math.floor(Math.random() * 101));
-    }
-    if(message.content.startsWith(`${PREFIX}anime`)) 
-    {
-        anime.anime(bot, msgs, PREFIX, message)
-    }
-    if(message.content.startsWith(`${PREFIX}addanime`))
-    {
-        anime.addanime(bot, msgs, PREFIX, message)
-    }
-    if(message.content.startsWith(`${PREFIX}delanime`))
-    {
-        anime.delanime(bot, msgs, PREFIX, message)
-    }
-    if(message.content.toString() === `${PREFIX}myanimelist`)
-    {
-        anime.myanimelist(bot, msgs, PREFIX, message)
-    }
-    if(message.content.startsWith(`${PREFIX}cctl`))  
-    {
-        cctl.cctlRandom(bot, msgs, message)
-    }
-    //Delete X message on the current channel.
-    if(message.content.startsWith(`${PREFIX}prune`))
-    {
-        prune.prune(bot, msgs, message)
-    }
-    if(message.content.toString() === `${PREFIX}mymemo`)
-    {
-        memo.mymemo(bot, msgs, message)
-    }
-    if(message.content.startsWith(`${PREFIX}addmemo`))
-    {
-        memo.addmemo(bot, msgs, message, PREFIX)
-    }
-    if(message.content.startsWith(`${PREFIX}delmemo`))
-    {
-        memo.delmemo(bot, msgs, PREFIX, message)
-    }
-    if(message.content.toString() === `${PREFIX}help`)
-    {
-        help.help(bot, msgs, PREFIX)
-    }
-
-    if(message.content.toString() === `${PREFIX}yes`)
-    {
-        const name = 'Fugou Keiji: Balance:Unlimited'
-        // get data of an anime
-         malScraper.getInfoFromName(name)
-          .then((data) => { console.log(data.picture);
-                            message.channel.send(data.picture); })
-          .catch((err) => console.log(err))
-
-        // get list of aired on a particular anime 
-        malScraper.getEpisodesList(name)
-          .then((data) => console.log(data))
-          .catch((err) => console.log(err))
-
-    }
-
+var db = null;
+var collection = null;
+var collectionMemo = null;
+client.connect((err) => {
+  console.log('Connected to Atlas Mongo.');
+  db = client.db(NameDB);
+  collection = db.collection('test');
+  collectionMemo = db.collection('memo');
+  //collection.insertOne({ item: 'toto', qty: 15 });
 });
+bot.on('message', async (message) => {
+  function msgs(msg) {
+    message.channel.send(msg);
+  }
 
+  //¨PRIVATE MESSAGE
+  // erfffff
+  //bot.users.get("157510824426995714").send("poulet")
+  // hiroi
+  //bot.users.get("160835265928232961").send("poulet")
+
+  if (!message.content.startsWith(PREFIX)) return;
+  if (message.author.bot) return;
+  /* Restart the bot */
+  if (message.content.toString() === `${PREFIX}rs`) {
+    restart.rs(bot, msgs, message);
+  }
+  /* Test */
+  if (message.content.toString() === `${PREFIX}test`) {
+    let test = 'Ceci est un test. oui';
+    msgs(test);
+  }
+  /* Rnadom number between 0 and 100 */
+  if (message.content.toString() === `${PREFIX}random`) {
+    msgs(Math.floor(Math.random() * 101));
+  }
+  /* REWORK */
+  if (message.content.startsWith(`${PREFIX}anime`)) {
+    anime.anime(bot, msgs, PREFIX, message);
+  }
+  /* REWORK */
+  if (message.content.startsWith(`${PREFIX}addanime`)) {
+    anime.addanime(bot, msgs, PREFIX, message);
+  }
+  /* REWORK */
+  if (message.content.startsWith(`${PREFIX}delanime`)) {
+    anime.delanime(bot, msgs, PREFIX, message);
+  }
+  /* REWORK */
+  if (message.content.toString() === `${PREFIX}myanimelist`) {
+    anime.myanimelist(bot, msgs, PREFIX, message);
+  }
+  /* Answer the cctl question. 100% acuracy */
+  if (message.content.startsWith(`${PREFIX}cctl`)) {
+    cctl.cctlRandom(bot, msgs, message);
+  }
+  /* Delete X message on the current channel. */
+  if (message.content.startsWith(`${PREFIX}prune`)) {
+    prune.prune(bot, msgs, message);
+  }
+  if (message.content.startsWith(`${PREFIX}mymemo`)) {
+    memo.mymemo(bot, msgs, message, PREFIX, db);
+  }
+  if (message.content.startsWith(`${PREFIX}addmemo`)) {
+    memo.addmemo(bot, msgs, message, PREFIX, db);
+  }
+  if (message.content.startsWith(`${PREFIX}delmemo`)) {
+    memo.delmemo(bot, msgs, PREFIX, message, db);
+  }
+  /* Show all the commands */
+  if (message.content.toString() === `${PREFIX}help`) {
+    help.help(bot, msgs, PREFIX);
+  }
+
+  /* TEST */
+  if (message.content.toString() === `${PREFIX}yes`) {
+    const name = 'Fugou Keiji: Balance:Unlimited';
+    // get data of an anime
+    malScraper
+      .getInfoFromName(name)
+      .then((data) => {
+        console.log(data.picture);
+        message.channel.send(data.picture);
+      })
+      .catch((err) => console.log(err));
+
+    // get list of aired on a particular anime
+    malScraper
+      .getEpisodesList(name)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }
+  /* Reset the DB */
+  if (message.content.toString() === `${PREFIX}resetDB`) {
+    collectionMemo.drop();
+    db.createCollection('memo');
+  }
+});
+//client.close();
 bot.music.start(bot, {
   // Set the api key used for YouTube.
   // This is required to run the bot.
@@ -159,9 +159,9 @@ bot.music.start(bot, {
   // The PLAY command Object.
   play: {
     // Usage text for the help command.
-    usage: "{{PREFIX}}play Youtube_links",
+    usage: '{{PREFIX}}play Youtube_links',
     // Whether or not to exclude the command from the help command.
-    exclude: false  
+    exclude: false,
   },
 
   // Make it so anyone in the voice channel can skip the
@@ -170,13 +170,13 @@ bot.music.start(bot, {
 
   // Make it so the owner (you) bypass permissions for music.
   ownerOverMember: true,
-  ownerID: "157510824426995714",
+  ownerID: '157510824426995714',
 
   // The cooldown Object.
   cooldown: {
     // This disables the cooldown. Not recommended.
-    enabled: false
-  }
+    enabled: false,
+  },
 });
 
 bot.login(bot_settings.token);
@@ -200,4 +200,3 @@ if(((splitAnime[0] == "!anime") && (splitAnime[1] != null)) && (splitAnime[2] !=
     // !anime Tensei
     console.log("je suis juste !anime anime anime")
 }*/
-
--- memo.js
+++ memo.js
@@ -1,141 +1,201 @@
-var fs = require("fs");
+var fs = require('fs');
 
-exports.mymemo = (bot, msg, message) =>
-{
-	(async () => 
-    {
-	    let user_id_memo = message.author.id;
-        // Read file
-        let contentGetMemo = fs.readFileSync('./DatabaseList/ListeMemo.json');
-        // Transorm json file into array
-        let parsedGetMemo = JSON.parse(contentGetMemo);
-        let list_memo = []
-
-        for (let i=0; i<parsedGetMemo.length; i++)
+exports.mymemo = (bot, msg, message, PREFIX, db) => {
+  (async () => {
+    let user_id_mymemo = message.author.id;
+    let list_memo = [];
+    // GET message
+    let Mymemo = message.content;
+    // Split message and get last word the user entered
+    let sentenceMymemo = Mymemo.split(' ');
+    // show a certain memo
+    if (sentenceMymemo[0] == `${PREFIX}mymemo` && sentenceMymemo[1] != null) {
+      db.collection('memo').findOne(
         {
-            if(parsedGetMemo[i].user_id == user_id_memo)
-            {
-                list_memo.push({name: "**" + parsedGetMemo[i].name_memo + "**", value: parsedGetMemo[i].content_memo})
-            }
+          user_id: user_id_mymemo,
+          title: sentenceMymemo[1],
+        },
+        function (err, result) {
+          if (err) throw err;
+          // memo exist.
+          if (result) {
+            list_memo.push({
+              name: '**' + result.title + '**',
+              value: result.content,
+            });
+            msg({
+              embed: {
+                color: 3447003,
+                author: {
+                  name: bot.user.username,
+                  icon_url: bot.user.avatarURL,
+                },
+                fields: list_memo,
+                timestamp: new Date(),
+                footer: {
+                  icon_url: bot.user.avatarURL,
+                  text: '©',
+                },
+              },
+            });
+          } else {
+            msg('Memo doesnt exist.');
+          }
         }
-
-        msg({embed: 
-        {
-            color: 3447003,
-            author: {
-              name: bot.user.username,
-              icon_url: bot.user.avatarURL
-            },
-            fields: list_memo,
-            timestamp: new Date(),
-            footer: {
-              icon_url: bot.user.avatarURL,
-              text: "©"
+      );
+    }
+    // show all the memo (without the content)
+    if (sentenceMymemo[0] == `${PREFIX}mymemo` && sentenceMymemo[1] == null) {
+      db.collection('memo')
+        .find({ user_id: user_id_mymemo })
+        .toArray(function (err, result) {
+          if (err) throw err;
+          if (result != []) {
+            for (let index = 0; index < result.length; index++) {
+              console.log(result[index].title);
+              list_memo.push(` => ${result[index].title} \n`);
             }
-        }})
-	})();
-}
-exports.addmemo = (bot, msg, message, PREFIX) =>
-{
-	(async () => 
-    {
-        // GET user id
-        let user_id_addMemo = message.author.id;
-        // GET message
-        let AddMemo = message.content;
-        // Split message and get last word the user entered
-        let sentenceAddMemo = AddMemo.split(" ");
-        let list_AddMemo=[];
-        // Send Message to a price User.
-        /*
+            // .join : array to string, .replace : on enleve TOUTES les virgules.
+            let stringval = list_memo.join().replace(/,/g, ' ');
+            msg({
+              embed: {
+                color: 3447003,
+                author: {
+                  name: bot.user.username,
+                  icon_url: bot.user.avatarURL,
+                },
+                fields: [
+                  {
+                    name: 'List de tes Mémos perso : ',
+                    value: stringval,
+                  },
+                ],
+                timestamp: new Date(),
+                footer: {
+                  icon_url: bot.user.avatarURL,
+                  text: '©',
+                },
+              },
+            });
+          } else {
+            msg('aucun item dans ta liste perso de mémo.');
+          }
+        });
+    }
+  })();
+};
+exports.addmemo = (bot, msg, message, PREFIX, db) => {
+  (async () => {
+    // GET user id
+    let user_id_addMemo = message.author.id;
+    // GET message
+    let AddMemo = message.content;
+    // Split message and get last word the user entered
+    let sentenceAddMemo = AddMemo.split(' ');
+    // Send Message to a certain User.
+    /*
         let MessageUser = bot.users.get("ID USER");
         MessageUser.send("You need to wake up my friend! your id is : " + "ID USER");
-        */
+    */
 
-        if((sentenceAddMemo[0] == `${PREFIX}addmemo`) && (sentenceAddMemo[1] != null)) 
+    if (
+      sentenceAddMemo[0] == `${PREFIX}addmemo` &&
+      sentenceAddMemo[1] != null &&
+      sentenceAddMemo[2] != null
+    ) {
+      // check if a memo with the same title exist
+      db.collection('memo').findOne(
         {
-            // Read file
-            let contentGetMemo = fs.readFileSync('./DatabaseList/ListeMemo.json');
-            // Transorm json file into array
-            let parsedGetMemo = JSON.parse(contentGetMemo);
-
-            let bool = 1;
-            
-            let contentMemo = "";
-            for (let h=2; h<sentenceAddMemo.length; h++)
-            {
-                tempMemo = sentenceAddMemo[h];
-                contentMemo = contentMemo + " " + tempMemo;
+          user_id: user_id_addMemo,
+          title: sentenceAddMemo[1],
+        },
+        function (err, result) {
+          if (err) throw err;
+          // memo doesnt exist, continue.
+          if (!result) {
+            // get all the content inside a variable
+            let contentMemo = '';
+            for (let h = 2; h < sentenceAddMemo.length; h++) {
+              tempMemo = sentenceAddMemo[h];
+              contentMemo = contentMemo + ' ' + tempMemo;
             }
-            
-            for (let m=0; m<parsedGetMemo.length; m++)
-            {
-                if((sentenceAddMemo[1].toLowerCase() == parsedGetMemo[m].name_memo.toLowerCase()) && (parsedGetMemo[m].user_id == user_id_addMemo))
-                {  
-                    msg("tutututu kestufé, tu as déjà ajouté ce mémo à ta liste perso, essaie pas de m'arnaquer.");
-                    bool = 0;
-                }
+            // verify the length before inserting the data in mongo
+            if (
+              sentenceAddMemo[1].length <= 256 &&
+              contentMemo.length <= 1024
+            ) {
+              // write data inside mongo
+              db.collection('memo').insertOne({
+                user_id: user_id_addMemo,
+                title: sentenceAddMemo[1],
+                content: contentMemo,
+              });
+              msg(
+                '<@!' +
+                  user_id_addMemo +
+                  '>, ' +
+                  'Le mémo **' +
+                  sentenceAddMemo[1] +
+                  '** avec le contenu : **' +
+                  contentMemo +
+                  '** à bien été ajouté à ta liste personelle !'
+              );
+            } else {
+              // length error.
+              msg(
+                "tutututu kestufé, ta cru tu voulais réécrire la bible fdp, fais un titre et un content plus cours sale chien, pour un titre c'est max 256 characteres et la t'en as " +
+                  sentenceAddMemo[1].length +
+                  " et dans le content c'est max 1024 charactères et la t'en as " +
+                  contentMemo.length
+              );
             }
-            if((sentenceAddMemo[1].length <=256) && (contentMemo.length <=1024))
-            {
-                if(bool == 1)
-                {
-                    let contentAddMemo = fs.readFileSync('./DatabaseList/ListeMemo.json');
-                    let parsedAddMemo = JSON.parse(contentAddMemo);
-                    let list_Memo = { user_id: user_id_addMemo, name_memo: sentenceAddMemo[1], content_memo: contentMemo}; 
-                    parsedAddMemo.push(list_Memo);
-                    let JSON_memo = JSON.stringify(parsedAddMemo);
-                    fs.writeFile("./DatabaseList/ListeMemo.json", JSON_memo, function(err, result) {
-                        if(err) console.log('error', err);
-                    });
-                    bool = 1;
-                    msg("<@!" +  user_id_addMemo + ">, " + "Le mémo **" + sentenceAddMemo[1] + "** avec le contenu : **" + contentMemo + "** à bien été ajouté à ta liste personelle !");
-                }                
-            }
-            else
-            {
-                msg("tutututu kestufé, ta cru tu voulais réécrire la bible fdp, fais un titre et un content plus cours sale chien, pour un titre c'est max 256 characteres et la t'en as " + sentenceAddMemo[1].length + " et dans le content c'est max 1024 charactères et la t'en as " + contentMemo.length);
-            }
-        }   
-	})();
-}
-exports.delmemo = (bot, msg, PREFIX, message) =>
-{
-	(async () => 
-    {
-    	        // Get user id
-        let user_id_delmemo = message.author.id;
-        // Read file
-        let contentGetMemo = fs.readFileSync('./DatabaseList/ListeMemo.json');
-        // Transorm json file into array
-        let parsedGetMemo = JSON.parse(contentGetMemo);
-        // GET message
-        let DelMemo = message.content;
-        // Split message and get last word the user entered
-        let sentenceDelMemo = DelMemo.split(" ");
-
-        let bool = 0;
-
-        if((sentenceDelMemo[0] == `${PREFIX}delmemo`) && (sentenceDelMemo[1] != null) && (sentenceDelMemo[2] == null))
-        {
-            for (let m=0; m<parsedGetMemo.length; m++)
-            {
-                if((sentenceDelMemo[1].toLowerCase() == parsedGetMemo[m].name_memo.toLowerCase()) && (parsedGetMemo[m].user_id == user_id_delmemo))
-                {                    
-                    msg("<@!" +  user_id_delmemo + ">, " + "Le Mémo " + parsedGetMemo[m].name_memo + " à bien été supprimer de ta liste personelle !");
-                    parsedGetMemo.splice(m, 1);
-                    let JSON_Memo = JSON.stringify(parsedGetMemo);
-                    fs.writeFile("./DatabaseList/ListeMemo.json", JSON_Memo, function(err, result) {
-                        if(err) console.log('error', err);
-                    });
-                    bool = 1;
-                    break;
-                }
-                if ((bool == 0) && (m == parsedGetMemo.length - 1))
-                {
-                    msg("Déso gros mais tu essaye de supprimer un animé n'étant pas dans ta liste personelle. (spoiler : du coup tu supprime R)");
-                }
-            }
+          } else {
+            // title already exist, error.
+            msg(
+              "tutututu kestufé, tu as déjà ajouté ce mémo à ta liste perso, essaie pas de m'arnaquer."
+            );
+          }
         }
-	})();
-}
+      );
+    }
+  })();
+};
+exports.delmemo = (bot, msg, PREFIX, message, db) => {
+  (async () => {
+    // Get user id
+    let user_id_delmemo = message.author.id;
+    // GET message
+    let DelMemo = message.content;
+    // Split message and get last word the user entered
+    let sentenceDelMemo = DelMemo.split(' ');
+    if (
+      sentenceDelMemo[0] == `${PREFIX}delmemo` &&
+      sentenceDelMemo[1] != null
+    ) {
+      // delete the memo
+      db.collection('memo').deleteOne(
+        {
+          user_id: user_id_delmemo,
+          title: sentenceDelMemo[1],
+        },
+        function (err, obj) {
+          if (err) throw err;
+          // check if the title exist
+          if (obj.result.n == 0) {
+            msg('ya R a delete vu que le mémo existe pas bouffon');
+          } else {
+            // send message to confirm that the memo is deleted
+            msg(
+              '<@!' +
+                user_id_delmemo +
+                '>, ' +
+                'Le Mémo ' +
+                sentenceDelMemo[1] +
+                ' à bien été supprimer de ta liste personelle !'
+            );
+          }
+        }
+      );
+    }
+  })();
+};
